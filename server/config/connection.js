c69c7f03bccc427fec8f21d4cbdb8010bcbf3268
kemaldemirgil <kemal.demirgil@hotmail.com>
2021-07-21 15:14:13 -0400

    seeding issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/amazoniaDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/amazoniaDBatlas', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
